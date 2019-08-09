64d82131e5fe5c2d27fbd1ea93840b203e81a8a5
Josephine Geoghegan <josephineanngeoghegan@gmail.com>
2019-08-09 18:25:51 +0200

    image uploader

--- db/migrate/20190808094845_create_cocktails.rb
+++ db/migrate/20190808094845_create_cocktails.rb
@@ -2,7 +2,6 @@ class CreateCocktails < ActiveRecord::Migration[5.2]
   def change
     create_table :cocktails do |t|
       t.string :name
-      mount_uploader :photo, PhotoUploader
 
       t.timestamps
     end
